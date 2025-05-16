c847cdad996080b99fd7e11823088556460fbd46
Fabricio Maciel <fabriciomaciel.dev@gmail.com>
2025-05-16 01:05:25 -0300

    feat: area de recorde

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,13 +4,15 @@ const state = {
     enemy: document.querySelector(".enemy"),
     timeLeft: document.querySelector("#time-left"),
     score: document.querySelector("#score"),
+    record: document.querySelector("#record"),
   },
   values: {
     timeId: null,
     gameVelocity: 500,
     hitPosition: 0,
     result: 0,
-    currentTime: 60,
+    currentTime: 10,
+    recordAtual: 0,
   },
   actions: {
     countDownTimerId: setInterval(countDown, 1000),
@@ -20,18 +22,22 @@ const state = {
 // Função que toca o som de acerto
 function playSound() {
   let audio = new Audio("./src/audios/hit.m4a");
-  audio.volume = 0.04;
+  audio.volume = 0.009;
   audio.play();
 }
 
-// Função para correr o tempo de forma interna e visual
+// Função para correr o tempo de forma interna, visual e finalizar o game
 function countDown() {
   state.values.currentTime--;
   state.view.timeLeft.textContent = state.values.currentTime;
 
   if (state.values.currentTime <= 0) {
     clearInterval(state.actions.countDownTimerId);
-    alert("Game Over! O seu resultado foi: " + state.values.result);
+    clearInterval(state.values.timeId);
+    if (state.values.result > state.values.recordAtual) {
+      state.values.recordAtual = state.values.result;
+      state.view.record.textContent = `RECORD - ${state.values.recordAtual} `;
+    }
   }
 }
 
